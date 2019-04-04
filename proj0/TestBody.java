c86f1fcf262b7e91434d2891922a39970b43c76f
YJZFlora <florayjz@gmail.com>
2019-04-03 19:06:41 -0700

    TestBody unifished

---
+++ proj0/TestBody.java
@@ -0,0 +1,10 @@
+public class TestBody{
+  public static void main(String[] args){
+    go();
+  }
+  private static void go(){
+    System.out.println("Checking Body...");
+
+    Body b1 = new Body(1.0, 3.0, 4.0, 5.0, "jupiter.gif");
+  }
+}
