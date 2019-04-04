e46154b049d3be460f6a62d6c67dd879703f1c85
YJZFlora <florayjz@gmail.com>
2019-04-04 14:53:54 -0700

    finished readBodies

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,10 +1,35 @@
 public class NBody{
 
-  public static void main(String[] args) {
-
+  public static double readRadius(String filename){
+    In in = new In(filename);
+    double number = 0.0;
+    for(int i=0; i<2; i++){
+     number = in.readDouble();
+    }
+    return number;
   }
 
-  private static double readRadius(String filename){
+  public static Body[] readBodies(String filename){
     In in = new In(filename);
+    int planetNumber = in.readInt();
+    Body[] bodyArray = new Body[planetNumber];
+    in.readDouble();
+    double xxPos;
+    double yyPos;
+    double xxVel;
+    double yyVel;
+    double mass;
+    String img;
+    for(int i=0; i<planetNumber;i++){
+      xxPos = in.readDouble();
+      yyPos = in.readDouble();
+      xxVel = in.readDouble();
+      yyVel = in.readDouble();
+      mass = in.readDouble();
+      img = in.readString();
+      bodyArray[i] = new Body(xxPos, yyPos, xxVel, yyVel, mass, img);
+    }
+    return bodyArray;
+  }
 
 }
